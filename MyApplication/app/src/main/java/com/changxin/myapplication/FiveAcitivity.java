0fb48a2f0bcb355c661578b7fce150cf2938e97b
xingege662 <924717778@qq.com>
2016-12-27 13:52:00 +0800

    ConstriantLayout学习

---
+++ MyApplication/app/src/main/java/com/changxin/myapplication/FiveAcitivity.java
@@ -0,0 +1,13 @@
+package com.changxin.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FiveAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_five_acitivity);
+    }
+}
